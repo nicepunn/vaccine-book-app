de7013d4bb6d206ef137baf493b6d1eae61c69e7
Punnawat Lohanuit <punnawat58000@gmail.com>
2023-11-02 23:24:03 +0700

    HW12

--- src/app/(hospitalinfo)/hospital/page.tsx
+++ src/app/(hospitalinfo)/hospital/page.tsx
@@ -1,19 +1,30 @@
 import CardPanal from "@/components/CardPanel";
-import getHospital from "@/libs/getHospitals";
+import getHospitals from "@/libs/getHospitals";
 import HospitalCatalog from "@/components/HospitalCatalog";
 import { Suspense } from "react";
 import { LinearProgress } from "@mui/material";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import addHospitalForm from "@/components/addHospitalForm";
+import getUserProfile from "@/libs/getUserProfile";
+import { getServerSession } from "next-auth";
 
-export default function Hospital() {
 
-    const hospitals = getHospital()
+export default async function Hospital() {
 
+    const hospitals = getHospitals()
+    const session = await getServerSession(authOptions);
+	const profile = session ? await getUserProfile(session.user.token) : null;
 
     return (
         <main className='text-center p-5'>
             <h1 className="text-xl">Select hospital</h1>
             <Suspense fallback={<p>Loading ... <LinearProgress/></p>}>
                 <HospitalCatalog hospitalJson={hospitals}/>
+                    {
+                        (profile?.data.role == "admin")?
+                        <addHospitalForm/>
+                        :null
+                    }
             </Suspense>
 
             {/* <hr className="my-10"/>
